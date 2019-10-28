9afae9df941aa98ae5aae6751067433df3e66730
henning.bunk <henning.bunk@wetteronline.de>
2019-10-28 19:44:06 +0100

    fill values into templates

--- Main.py
+++ Main.py
@@ -1,5 +1,29 @@
 import json
 import Templates
+import re
+
+def main():
+    # Read in Stories and metadata
+
+    inputFilenames = ["story/story-template.json"] # TODO use a list with all input files here
+    stories = readInJsons(inputFilenames)
+
+    for story in stories:
+        chapterCount = len(story["book"])
+
+        print(story)
+        # Title page
+        writeTitlePage(1, story)
+        # Preface
+        # f = open('Section0002.xhtml', 'w')
+        # f.write(message)
+        # f.close()
+        # # Main story
+        # f = open('Section0003.xhtml', 'w')
+        # f.write(message)
+        # f.close()
+
+    # Export ebook files
 
 
 def readInJsons(inputFilenames):
@@ -15,31 +39,33 @@ def writeTitlePage(index, json):
     f = open('Section0001.xhtml', 'w')
     # print '<a href="%(url)s">%(url)s</a>' % {'url': my_url}
     values = {
-        'title': json["title-original"]
+        'title-german': json["title-german"],
+        'title-german-slug': toSlug(json['title-german']),
+        'title-original': json["title-original"],
+        'year': json['year'],
+        'published': json['published'],
+        'translation': json['translation'],
+        'source': json['source'],
+        'category': json['category'],
+        'content': getTitlePageContent(json)
     }
     message = Templates.formatTitlePage(values)
     f.write(message)
     f.close()
 
 
-# Read in Stories and metadata
+def toSlug(string):
+    return re.sub('[ #`\'"?!&()]', '-', string).lower()
 
-inputFilenames = ["story/story-template.json"]
-stories = readInJsons(inputFilenames)
 
-for story in stories:
-    chapterCount = len(story["book"])
+def getTitlePageContent(json):
+    # TODO check if there should be a TOC, than create one
+    if hasChapters(json):
 
-    print(story)
-    # Title page
-    writeTitlePage(1, story)
-    # Preface
-    f = open('Section0002.xhtml', 'w')
-    f.write(message)
-    f.close()
-    # Main story
-    f = open('Section0003.xhtml', 'w')
-    f.write(message)
-    f.close()
+    if hasPreface(json):
+    values = {
 
-# Export ebook files
+    }
+    return Templates.content(values)
+
+main()
